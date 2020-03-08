b28c56bf6dba5837aa5b99ae7ea4b7e0a0408f2d
Logan Markham <MARKHAML@ETSU.EDU>
2020-03-08 18:46:27 -0400

    - Restructured previous commit for Logging functionality. - Added an example of how data will be logged for page clicks inside of BicycleController. (Need to determine which pages we want to log information for when clicked.)

---
+++ Bikeshop-Project/Bikeshop-Project/LoggerModels/Logs.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bikeshop_Project.LoggerModels
+{
+    public class Logs
+    {
+        [Key]
+        public int Id { get; private set; }
+        public DateTime TIME_STAMP { get; private set; }
+        public string UserName { get; private set; }
+        public DateTime TimeLoggedIn { get; private set; }
+        public DateTime TimeLoggedOut { get; private set; }
+        public int NumberOfPageViews { get; private set; }
+        public TimeSpan SessionDuration { get; private set; }
+        public string PageTitle { get; private set; }
+
+        public Logs(int Id, DateTime TIME_STAMP, string UserName, DateTime TimeLoggedIn, DateTime TimeLoggedOut, int NumberOfPageViews, TimeSpan SessionDuration, string PageTitle)
+        {
+            this.Id = Id;
+            this.TIME_STAMP = TIME_STAMP;
+            this.UserName = UserName;
+            this.TimeLoggedIn = TimeLoggedIn;
+            this.TimeLoggedOut = TimeLoggedOut;
+            this.NumberOfPageViews = NumberOfPageViews;
+            this.SessionDuration = SessionDuration;
+            this.PageTitle = PageTitle;
+        }
+    }
+}
---
+++ Bikeshop-Project/Bikeshop-Project/LoggerModels/User.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Bikeshop_Project.LoggerModels
+{
+    public class User
+    {
+        [Key]
+        public string UserName { get; private set; }
+        public string IPAddress { get; private set; }
+        public string Country { get; private set; }
+        public string City { get; private set; }
+
+        public User(string UserName, string IPAddress, string Country, string City)
+        {
+            this.UserName = UserName;
+            this.IPAddress = IPAddress;
+            this.Country = Country;
+            this.City = City;
+        }
+    }
+}
