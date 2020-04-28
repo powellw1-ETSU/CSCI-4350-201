e25d616765e647b94a5ff57117aa1af33bbe5b5d
David <Dbeverley7@gmail.com>
2020-04-28 10:49:17 -0400

    Added Order View model, the create form for an order, and some controller detail (Not fully functional)

---
+++ Bikeshop-Project/Models/OrderMetric.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Bikeshop_Project.Models
+{
+    public class OrderMetric
+    {
+        public Decimal totalCost { get; set; }
+
+        public DateTime purchaseDate { get; set; }
+
+        public Decimal bicycleID { get; set; }
+
+        public Decimal customerID { get; set; }
+
+        
+    }
+}
