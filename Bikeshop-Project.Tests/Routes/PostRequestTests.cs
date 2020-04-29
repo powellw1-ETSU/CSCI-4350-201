33bc4ffb53948411f52e2b991c32b864c3a15feb
Logan Markham <MARKHAML@ETSU.EDU>
2020-04-29 14:57:50 -0400

    Changes to reflect new uri for service

---
+++ Bikeshop-Project.Tests/Routes/PostRequestTests.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+using Bikeshop_Project.LoggerModels;
+using Newtonsoft.Json;
+using Xunit;
+
+namespace Bikeshop_Project.Tests.Routes
+{
+    public class PostRequestTests
+    {
+        private readonly HttpClient _client;
+        private readonly string baseUriDB = "ec2-3-136-108-167.us-east-2.compute.amazonaws.com";
+        private readonly string baseUriMonitoring = "monitoringteamapi.azurewebsites.net/api";
+        private readonly string pageExtension = "pages";
+        private readonly string logsExtension = "logs";
+        private readonly string userExtension = "applicationusers";
+
+
+        /// <summary>
+        /// Constructor instantiates a new instance of the HttpClient used for testing
+        /// </summary>
+        public PostRequestTests()
+        {
+            _client = new HttpClient();
+        }
+
+        /// <summary>
+        /// This tests the monitoring team API accepts proper POST request for "Logs" objects.
+        /// These objects contain metrics for a general user session
+        /// </summary>
+        [Fact]
+        public async void PostRequest_MonitoringTeam_Logs_Correct()
+        {
+            // Arrange
+            Logs testLog = new Logs(DateTime.Now.ToString(),
+                                    "this is from testing",
+                                    DateTime.Now,
+                                    DateTime.Now,
+                                    0,
+                                    "Sample page title");
+
+            string uri = $"http://{baseUriMonitoring}/{logsExtension}";
+            var json = JsonConvert.SerializeObject(testLog);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response;
+
+            // Act
+            response = await _client.PostAsync(uri, content);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+        }
+
+        /// <summary>
+        /// This test ensures the monitoring team API is accepting a properly format POST request for a Page metric
+        /// </summary>
+        [Fact]
+        public async void PostRequest_MonitoringTeam_Pages_Correct()
+        {
+            PageInfo test = new PageInfo(DateTime.Now.ToString(), "this is a test", "Sample username for test");
+
+            string uri = $"http://{baseUriMonitoring}/{pageExtension}";
+            var json = JsonConvert.SerializeObject(test);
+            var content = new StringContent(json, Encoding.UTF8, "application/json");
+            HttpResponseMessage response;
+
+            // Act
+            response = await _client.PostAsync(uri, content);
+
+            // Assert
+            Assert.Equal(HttpStatusCode.Created, response.StatusCode);
+
+        }
+    }
+}
